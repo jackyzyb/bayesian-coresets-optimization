e937dccb9e33051befd7f93d59133b26ae044c33
Jacky Zhang <jacky@Jackys-MacBook-Pro.local>
2021-04-08 01:39:35 -0500

    add simplex constraint

--- IHT_toolbox/accelerated_iht.py
+++ IHT_toolbox/accelerated_iht.py
@@ -4,17 +4,21 @@ Bayesian Coresets: Revisiting the Nonconvex Optimization Perspective (https://ar
 Jacky Y. Zhang, Rajiv Khanna, Anastasios Kyrillidis, and Oluwasanmi Koyejo. (AISTATS 2021)
 
 Both numpy version and pytorch version are offered, where the torch version can be run on GPU for acceleration.
-4 functions are included:
+6 functions are included:
 iht_obj(y, A, w):                                                   calculate the objective value
-a_iht_i(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):         A-IHT I implemented by numpy
-a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):        A-IHT II implemented by numpy
-a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):  A-IHT II implemented by torch
+l2_projection_torch(w, K, L=None, already_K_sparse=False, K_sparse_supp=None)       l2 projection implemented by numpy
+l2_projection_torch(w, K, L=None, already_K_sparse=False, K_sparse_supp=None)       l2 projection implemented by torch
+a_iht_i(y, A, K, tol=1e-5, max_iter_num=300, verbose=True, L=None):         A-IHT I implemented by numpy
+a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True, L=None):        A-IHT II implemented by numpy
+a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True, L=None):  A-IHT II implemented by torch
 
 The optimization objective is
-    argmin_w ||y - Aw||^2    s.t.    |w|_0 <= K    and     w >= 0
+    argmin_w ||y - Aw||^2    s.t.    ||w||_0 <= K    and    w >= 0    (optional: and sum(w) = L)
         where   y is of shape (M, 1),
                 A is of shape (M, N),
-                K is an positive integer
+                w is of shape (N, 1),
+                K is a positive integer,
+                L is a positive number.
 """
 
 import numpy as np
@@ -23,7 +27,7 @@ import torch
 
 def iht_obj(y, A, w):
     """
-    calculate the quadratic objective value given w
+    Calculate the quadratic objective value given w
     :param y: numpy.ndarray of shape (M, 1)
     :param A: numpy.ndarray of shape (M, N)
     :param w: numpy.ndarray of shape (N, 1)
@@ -32,7 +36,123 @@ def iht_obj(y, A, w):
     return np.linalg.norm(y - A.dot(w), ord=2)
 
 
-def a_iht_i(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
+def l2_projection_numpy(w, K, L=None, already_K_sparse=False, K_sparse_supp=None):
+    """
+    If L is None, project w to the K-sparsity constrained and non-negative region;
+    if L is not None, project w the K-sparsity constrained, non-negative and the sum(w) = L region;
+    the projection is optimal in l2 distance.
+    For the projection with L, see the paper
+    Sparse projections onto the simplex (https://arxiv.org/pdf/1206.1529.pdf).
+    Anastasios Kyrillidis, Stephen Becker, Volkan Cevher, Christoph Koch (ICML 2013)
+    :param w: numpy.ndarray of shape (N, 1)
+    :param K: int (sparsity constraint), positive
+    :param L: float, positive
+    :param already_K_sparse: bool. If the input w has been already K-sparse, put 'True' to for a faster projection
+    :param K_sparse_supp: list. If the input w has been already K-sparse, put its support here
+    :return: w: numpy.ndarray of shape (N, 1). A new vector that is the projected w
+             selected_support: list of integer indexes (the support of the w).
+    """
+    N = w.shape[0]
+    if L is None:
+        if already_K_sparse:
+            w_projected = w.copy()
+            w_projected[w_projected < 0] = 0
+            return w_projected, K_sparse_supp
+        else:
+            index_w = np.flip(np.argsort(np.squeeze(w)))
+            index_w = np.squeeze(index_w).tolist()
+            w_projected = np.zeros([N, 1])
+            selected_support = index_w[0:K]
+            w_projected[selected_support] = w[index_w[0:K]]  # projection
+            w_projected[w_projected < 0] = 0  # truncate negative entries
+            return w_projected, selected_support
+    else:
+        if already_K_sparse:
+            w_selected = w[K_sparse_supp]
+        else:
+            index_w = np.flip(np.argsort(np.squeeze(w)))
+            index_w = np.squeeze(index_w).tolist()
+            K_sparse_supp = index_w[:K]
+            w_selected = w[K_sparse_supp]
+
+        w_projected = np.zeros([N, 1])
+        accumulate = 0
+        rho = 0
+        for j in range(K):
+            w_j = w_selected[j].item()
+            accumulate += w_j
+            if w_j > (accumulate - L) / (j + 1):
+                rho = j
+            else:
+                break
+        tau = (w_selected[:(rho + 1)].sum() - L) / (rho + 1)
+        w_selected = w_selected - tau
+        w_selected[w_selected < 0] = 0
+        w_projected[K_sparse_supp] = w_selected
+        return w_projected, K_sparse_supp
+
+
+def l2_projection_torch(w, K, L=None, already_K_sparse=False, K_sparse_supp=None):
+    """
+    If L is None, project w to the K-sparsity constrained and non-negative region;
+    if L is not None, project w the K-sparsity constrained, non-negative and the sum(w) = L region;
+    the projection is optimal in l2 distance.
+    For the projection with L, see the paper
+    Sparse projections onto the simplex (https://arxiv.org/pdf/1206.1529.pdf).
+    Anastasios Kyrillidis, Stephen Becker, Volkan Cevher, Christoph Koch (ICML 2013)
+    :param w: torch.tensor of shape (N, 1)
+    :param K: int (sparsity constraint), positive
+    :param L: float, positive
+    :param already_K_sparse: bool. If the input w has been already K-sparse, put 'True' to for a faster projection
+    :param K_sparse_supp: list. If the input w has been already K-sparse, put its support here
+    :return: w: torch.tensor of shape (N, 1). A new vector that is the projected w
+             selected_support: list of integer indexes (the support of the w).
+    """
+    device = w.device  # should be on the same device as A
+    dtype = w.dtype  # should be the same dtype as A
+    N = w.shape[0]
+    if L is None:
+        if already_K_sparse:
+            w_projected = w.clone()
+            w_projected[w_projected < 0] = 0
+            return w_projected, K_sparse_supp
+        else:
+            index_w = torch.argsort(w.squeeze())
+            index_w = index_w.flip(0)
+            index_w = index_w.squeeze().tolist()
+            w_projected = torch.zeros([N, 1], dtype=dtype, device=device)
+            selected_support = index_w[:K]
+            w_projected[selected_support] = w[selected_support]  # projection
+            w_projected[w_projected < 0] = 0  # truncate negative entries
+            return w_projected, selected_support
+    else:
+        if already_K_sparse:
+            w_selected = w[K_sparse_supp]
+        else:
+            index_w = torch.argsort(w.squeeze())
+            index_w = index_w.flip(0)
+            index_w = index_w.squeeze().tolist()
+            K_sparse_supp = index_w[:K]
+            w_selected = w[K_sparse_supp]
+
+        w_projected = torch.zeros([N, 1], dtype=dtype, device=device)
+        accumulate = 0
+        rho = 0
+        for j in range(K):
+            w_j = w_selected[j].item()
+            accumulate += w_j
+            if w_j > (accumulate - L) / (j + 1):
+                rho = j
+            else:
+                break
+        tau = (w_selected[:(rho + 1)].sum() - L) / (rho + 1)
+        w_selected = w_selected - tau
+        w_selected[w_selected < 0] = 0
+        w_projected[K_sparse_supp] = w_selected
+        return w_projected, K_sparse_supp
+
+
+def a_iht_i(y, A, K, tol=1e-5, max_iter_num=300, verbose=True, L=None):
     """
     A-IHT I implemented by numpy
     :param y: numpy.ndarray of shape (M, 1)
@@ -64,9 +184,9 @@ def a_iht_i(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
     complementary_Yi = np.ones([N, 1])
     i = 1
 
-    while (i <= max_iter_num):
+    while i <= max_iter_num:
         w_prev = w_cur
-        if (i == 1):
+        if i == 1:
             res = y
             der = A_t.dot(res)  # compute gradient
         else:
@@ -81,24 +201,18 @@ def a_iht_i(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
         Pder = A[:, S_i].dot(ider)
         mu_bar = ider.T.dot(ider) / Pder.T.dot(Pder) / 2  # step size selection
         b = y_cur + mu_bar * der  # gradient descent
-        ind_b = np.flip(np.argsort(np.squeeze(b)))
-        ind_b = np.squeeze(ind_b).tolist()
-        w_cur = np.zeros([N, 1])
-        S_i_temp = ind_b[0:K]
-        w_cur[S_i_temp] = b[ind_b[0:K]]  # projection
-        X_i = S_i_temp
-        w_cur[w_cur < 0] = 0  # truncate negative entries
+        w_cur, X_i = l2_projection_numpy(b, K, L=L)
 
         A_w_cur = A[:, X_i].dot(w_cur[X_i])
         res = y - A_w_cur
 
-        if (i == 1):
+        if i == 1:
             A_diff = A_w_cur
         else:
             A_diff = A_w_cur - A_w_prev
 
         temp = A_diff.T.dot(A_diff)
-        if (temp > 0):
+        if temp > 0:
             tau = res.T.dot(A_diff) / temp
         else:
             tau = res.T.dot(A_diff) / 1e-6
@@ -117,13 +231,13 @@ def a_iht_i(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
 
     # finished
     w = w_cur
-    supp = np.nonzero(w_cur)[0].tolist()    # support of the output solution
+    supp = np.nonzero(w_cur)[0].tolist()  # support of the output solution
     print('Stopped at iteration {}. {} items are selected. The objective value is: {}'.format(i, len(supp),
                                                                                               iht_obj(y, A, w_cur)))
     return w, supp
 
 
-def a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
+def a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True, L=None):
     """
     A-IHT II implemented by numpy
     :param y: numpy.ndarray of shape (M, 1)
@@ -154,9 +268,9 @@ def a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
     complementary_Yi = np.ones([N, 1])
     i = 1
 
-    while (i <= max_iter_num):
+    while i <= max_iter_num:
         w_prev = w_cur
-        if (i == 1):
+        if i == 1:
             res = y
             der = A_t.dot(res)  # compute gradient
         else:
@@ -172,13 +286,8 @@ def a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
         Pder = A[:, S_i].dot(ider)
         mu_bar = ider.T.dot(ider) / Pder.T.dot(Pder) / 2  # step size selection
         b = y_cur + mu_bar * der  # gradient descent
-        ind_b = np.flip(np.argsort(np.squeeze(b)))
-        ind_b = np.squeeze(ind_b).tolist()
-        w_cur = np.zeros([N, 1])
-        S_i_temp = ind_b[0:K]
-        w_cur[S_i_temp] = b[ind_b[0:K]]  # projection
-        w_cur[w_cur < 0] = 0  # hard threshold negative entries
-        X_i = S_i_temp
+        w_cur, X_i = l2_projection_numpy(b, K, L=L)
+
         A_w_cur = A[:, X_i].dot(w_cur[X_i])
         res = y - A_w_cur
         der = A_t.dot(res)  # compute gradient
@@ -186,18 +295,18 @@ def a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
         Pder = A[:, X_i].dot(ider)
         mu_bar = ider.T.dot(ider) / Pder.T.dot(Pder) / 2  # step size selection
         w_cur[X_i] = w_cur[X_i] + mu_bar * ider  # debias
-        w_cur[w_cur < 0] = 0  # hard threshold negative entries
+        w_cur, _ = l2_projection_numpy(w_cur, K, already_K_sparse=True, K_sparse_supp=X_i, L=L)
 
         A_w_cur = A[:, X_i].dot(w_cur[X_i])
         res = y - A_w_cur
 
-        if (i == 1):
+        if i == 1:
             A_diff = A_w_cur
         else:
             A_diff = A_w_cur - A_w_prev
 
         temp = A_diff.T.dot(A_diff)
-        if (temp > 0):
+        if temp > 0:
             tau = res.T.dot(A_diff) / temp
         else:
             tau = res.T.dot(A_diff) / 1e-6
@@ -218,11 +327,11 @@ def a_iht_ii(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
     w = w_cur
     supp = np.nonzero(w_cur)[0].tolist()  # support of the output solution
     print('Stopped at iteration {}. {} items are selected. The objective value is: {}'.format(i, len(supp),
-                                                                                            iht_obj(y, A, w_cur)))
+                                                                                              iht_obj(y, A, w_cur)))
     return w, supp
 
 
-def a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
+def a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True, L=None):
     """
     A-IHT II implemented by pytorch
     :param y: torch.tensor of shape (M, 1)
@@ -234,8 +343,8 @@ def a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
     :return: w: torch.tensor of shape (N, 1)
              supp: list of integer indexes (the support of the w)
     """
-    device = y.device   # should be on the same device as A
-    dtype = y.dtype     # should be the same dtype as A
+    device = y.device  # should be on the same device as A
+    dtype = y.dtype  # should be the same dtype as A
     if verbose:
         print('running A-IHT II on {}'.format(device))
     (M, N) = A.shape
@@ -254,7 +363,7 @@ def a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
 
     for i in range(1, max_iter_num + 1):
         w_prev = w_cur
-        if (i == 1):
+        if i == 1:
             res = y
             der = A_t.mm(res)
         else:
@@ -264,20 +373,15 @@ def a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
         A_w_prev = A_w_cur
         complementary_Yi[Y_i] = 0
         ind_der = torch.argsort(torch.abs((der * complementary_Yi).squeeze()))
+        ind_der = ind_der.flip(0)
         complementary_Yi[Y_i] = 1
-        S_i = Y_i + (ind_der[-K:]).squeeze().tolist()
+        S_i = Y_i + (ind_der[:K]).squeeze().tolist()
         ider = der[S_i]
         Pder = A[:, S_i].mm(ider)
         mu_bar = ider.T.mm(ider) / Pder.T.mm(Pder) / 2
         b = y_cur + mu_bar * der
-        ind_b = torch.argsort(b.squeeze())
-        ind_b = ind_b.squeeze().tolist()
-        w_cur = torch.zeros([N, 1], dtype=dtype, device=device)
-        S_i_temp = ind_b[-K:]
-        w_cur[S_i_temp] = b[ind_b[-K:]]
-        w_cur[w_cur < 0] = 0    # hard threshold negative entries
+        w_cur, X_i = l2_projection_torch(b, K, L=L)
 
-        X_i = S_i_temp
         A_w_cur = A[:, X_i].mm(w_cur[X_i])
         res = y - A_w_cur
         der = A_t.mm(res)
@@ -285,24 +389,26 @@ def a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
         Pder = A[:, X_i].mm(ider)
         mu_bar = ider.T.mm(ider) / Pder.T.mm(Pder) / 2
         w_cur[X_i] = w_cur[X_i] + mu_bar * ider
-        w_cur[w_cur < 0] = 0    # hard threshold negative entries
+        w_cur, _ = l2_projection_torch(w_cur, K, already_K_sparse=True, K_sparse_supp=X_i, L=L)
 
         A_w_cur = A[:, X_i].mm(w_cur[X_i])
         res = y - A_w_cur
-        if (i == 1):
+        if i == 1:
             A_diff = A_w_cur
         else:
             A_diff = A_w_cur - A_w_prev
         temp = A_diff.T.mm(A_diff)
-        if (temp > 0):
+        if temp > 0:
             tau = res.T.mm(A_diff) / temp
         else:
             tau = res.T.mm(A_diff) / 1e-6
         y_cur = w_cur + tau * (w_cur - w_prev)
         Y_i = y_cur.squeeze().nonzero().squeeze().tolist()
+        if isinstance(Y_i, int):
+            Y_i = [Y_i]
 
         # print out objective function value during optimization of IHT
-        if (verbose and i % 50 == 1):
+        if verbose and i % 50 == 1:
             obj_value = torch.norm(y - A.mm(w_cur))
             print('at iteration {}, the objective value is: {}'.format(i, obj_value))
 
@@ -311,9 +417,9 @@ def a_iht_ii_torch(y, A, K, tol=1e-5, max_iter_num=300, verbose=True):
             break
 
     w = w_cur
-    supp = w_cur.squeeze().nonzero().squeeze().tolist()     # support of the output solution
+    supp = w_cur.squeeze().nonzero().squeeze().tolist()  # support of the output solution
+    if isinstance(supp, int):
+        supp = [supp]
     obj_value = torch.norm(y - A.mm(w_cur))
     print('Stopped at iteration {}. {} items are selected. The objective value is: {}'.format(i, len(supp), obj_value))
     return w, supp
-
-
--- IHT_toolbox/toolbox_examples.py
+++ IHT_toolbox/toolbox_examples.py
@@ -1,35 +1,48 @@
 import numpy as np
 import torch
-from accelerated_iht import iht_obj, a_iht_i, a_iht_ii, a_iht_ii_torch
+from accelerated_iht import *
 
 # settings
-M = 300
+M = 400
 N = 1000
 K = 100
+L = None  # None or a positive number
 np.random.seed(233)
-A = np.random.rand(M, N) - 0.5
+A = np.random.rand(M, N) + 0.5
 true_supp = np.random.permutation(N)[:K]
 true_w = np.zeros([N, 1])
 true_w[true_supp] = np.random.rand(K, 1)
 y = A.dot(true_w)
 
+# projection
+w = np.array([1, -1, -0.5, 0]).reshape([-1, 1])
+k_proj = 2
+w_projected, supp = l2_projection_numpy(w, k_proj, L=2)
+print('original w: {}. Sparsity: {}'.format(w, k_proj))
+print('projected w: {}'.format(w_projected))
+print('support: {}'.format(supp))
+w_projected, supp = l2_projection_numpy(w, k_proj, L=2, K_sparse_supp=[0, 1], already_K_sparse=True)
+print('projected w: {}'.format(w_projected))
+print('support: {}'.format(supp))
+
+
 # A-IHT I by numpy
 print('\nusing A-IHT I by numpy...')
-w, supp = a_iht_i(y, A, K)
-print('A-IHT I (numpy) finds solution with objective value {}\n'.format(iht_obj(y, A, w)))
+w, supp = a_iht_i(y, A, K, L=L)
+print('A-IHT I (numpy) finds solution with objective value {}; sum(w) is {}\n'.format(iht_obj(y, A, w), w.sum()))
 
 # A-IHT II by numpy
 print('using A-IHT II by numpy...')
-w, supp = a_iht_ii(y, A, K)
-print('A-IHT II (numpy) finds solution with objective value {}\n'.format(iht_obj(y, A, w)))
+w, supp = a_iht_ii(y, A, K, L=L)
+print('A-IHT II (numpy) finds solution with objective value {}; sum(w) is {}\n'.format(iht_obj(y, A, w), w.sum()))
 
 # A-IHT II by torch
 A = torch.tensor(A)
 y = torch.tensor(y)
 print('using A-IHT II by torch...')
-w, supp = a_iht_ii_torch(y, A, K)
+w, supp = a_iht_ii_torch(y, A, K, L=L)
 obj_value = torch.norm(y - A.mm(w))
-print('A-IHT II (torch) finds solution with objective value {}\n'.format(obj_value))
+print('A-IHT II (torch) finds solution with objective value {}; sum(w) is {}\n'.format(obj_value, w.sum()))
 
 
 
